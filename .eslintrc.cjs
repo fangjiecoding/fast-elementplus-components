2fb0627acbb6e36ca4b7e5a8a3c587bf578067a6
fangjie <857516512@qq.com>
2024-03-22 12:23:59 +0800

    ci: 修改配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,9 +7,11 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting',
-    'vue/multi-word-component-names':'off',
+    '@vue/eslint-config-prettier/skip-formatting'
   ],
+  rule: {
+    'vue/multi-word-component-names': 0
+  },
   parserOptions: {
     ecmaVersion: 'latest'
   }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import 'normalize.css'
 import App from './App.vue'
 import router from './router'
+
+import 'normalize.css'
+// @ts-ignore
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import mUI from 'fast-elementplus-components/menu'
 import 'fast-elementplus-components/menu/style.css'
+
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
